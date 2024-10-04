49de169727e7ad2a2c63ea935f52af901ba0c46a
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-10-05 01:14:24 +0600

    navbar desing completed with menu click dropdown and smooth animation as well

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -3,20 +3,13 @@ import React from 'react'
 
     interface ButtonProps {
         text: string,
-        buttonStyles: string,
-        hoverStyles: string,
+        styles: string,
     }
 
-    const Button: React.FC<ButtonProps> = ({ text,buttonStyles,hoverStyles }) => {
+    const Button: React.FC<ButtonProps> = ({ text,styles }) => {
         return (
-            // <div className={`${styles} px-3 py-2`}>
-            //     {text}
-            // </div>
-            <div className="group relative inline-block">
-                <button className={`${buttonStyles} relative z-10 px-6 py-2 font-semibold text-base rounded-lg overflow-hidden transition-all duration-300 ease-out`}>
-                    {text}
-                </button>
-                <div className={`${hoverStyles} absolute inset-0 z-0 rounded-lg transition-all duration-300 ease-out scale-0 group-hover:scale-100 group-hover:opacity-100`}></div>
+            <div className={`${styles} px-3 py-2 cursor-pointer transition-all duration-300 ease-out`}>
+                {text}
             </div>
         )
     }
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,37 +1,132 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io'
+
+
 
 const Navbar = () => {
+
+    const [openDropdown, setOpenDropdown] = useState<string | null>(null)
+
+
+    const handleClick = (dropdown:string) => {
+        setOpenDropdown(openDropdown === dropdown ? null : dropdown)
+    }
+
   return (
     <div className='bg-white'>
         <div className='py-4 w-4/5 mx-auto'>
-            <div className='flex justify-between'>
+            <div className='flex justify-between items-center'>
                 <div>
                     <Image src='/Logo.png' alt='logo' width={100} height={100} />
                 </div>
-                <div className='flex gap-6 items-center text-black'>
-                    <ul>
-                        <li><Link href='#'>Home</Link></li>
-                    </ul>
-                    <ul>
-                        <li><Link href='#'>Home</Link></li>
-                    </ul>
-                    <ul>
-                        <li><Link href='#'>Home</Link></li>
-                    </ul>
-                    <ul>
-                        <li><Link href='#'>Home</Link></li>
-                    </ul>
-                    <ul>
-                        <li><Link href='#'>Home</Link></li>
-                    </ul>
+                <div className=''>
+                    <div className='flex gap-16 items-center text-black'>
+                        <div className='relative'>
+                            <div onClick={()=>handleClick('home')} className='flex items-center gap-4 cursor-pointer'>
+                                <Link href='#'>Home</Link>
+                            </div>
+                        </div>
+                        <div className='relative'>
+                            <div onClick={()=>handleClick('services')} className='flex items-center gap-4 cursor-pointer'>
+                                <Link href='#'>Services</Link>
+                                {
+                                    openDropdown === 'services' ?
+                                        <IoIosArrowUp className="transition-transform duration-300 transform rotate-180" /> 
+                                            : 
+                                        <IoIosArrowUp className="transition-transform duration-300 transform rotate-0" />
+                                }
+                            </div>
+                            {openDropdown ==='services' &&
+                                <div className='absolute top-10 left-0 bg-red-100 p-3 transition-all duration-300 ease-in-out'>
+                                    <li>nice</li>
+                                    <li>nice</li>
+                                    <li>nice</li>
+                                </div>
+                            }
+                        </div>
+                        <div className='relative'>
+                            <div onClick={()=> handleClick('appexchange')} className='flex items-center gap-4 cursor-pointer'>
+                                <Link href='#'>AppExchange</Link>
+                                {
+                                    openDropdown === 'appexchange' ?
+                                        <IoIosArrowUp className="transition-transform duration-300 transform rotate-180" /> 
+                                            : 
+                                        <IoIosArrowUp className="transition-transform duration-300 transform rotate-0" />
+                                }
+                            </div>
+                            {openDropdown==='appexchange' &&
+                                <div className='absolute top-10 left-0 bg-red-100 p-3'>
+                                    <li>nice</li>
+                                    <li>nice</li>
+                                    <li>nice</li>
+                                </div>
+                            }
+                        </div>
+                        <div className='relative'>
+                            <div onClick={()=>handleClick('results')} className='flex items-center gap-4 cursor-pointer'>
+                                <Link href='#'>Results</Link>
+                                {
+                                    openDropdown === 'results' ?
+                                        <IoIosArrowUp className="transition-transform duration-300 transform rotate-180" /> 
+                                            : 
+                                        <IoIosArrowUp className="transition-transform duration-300 transform rotate-0" />
+                                }
+                            </div>
+                            {openDropdown === 'results' &&
+                                <div className='absolute top-10 left-0 bg-red-100 p-3'>
+                                    <li>nice</li>
+                                    <li>nice</li>
+                                    <li>nice</li>
+                                </div>
+                            }
+                        </div>
+                        <div className='relative'>
+                            <div onClick={()=>handleClick('aboutus')} className='flex items-center gap-4 cursor-pointer'>
+                                <Link href='#'>About Us</Link>
+                                {
+                                    openDropdown === 'aboutus' ?
+                                        <IoIosArrowUp className="transition-transform duration-300 transform rotate-180" /> 
+                                            : 
+                                        <IoIosArrowUp className="transition-transform duration-300 transform rotate-0" />
+                                }
+                            </div>
+                            {openDropdown === 'aboutus' &&
+                                <div className='absolute top-10 left-0 bg-red-100 p-3'>
+                                    <li>nice</li>
+                                    <li>nice</li>
+                                    <li>nice</li>
+                                </div>
+                            }
+                        </div>
+                        <div className='relative'>
+                            <div onClick={()=>handleClick('blogs')} className='flex items-center gap-4 cursor-pointer'>
+                                <Link href='#'>Blogs</Link>
+                                {
+                                    openDropdown === 'blogs' ?
+                                        <IoIosArrowUp className="transition-transform duration-300 transform rotate-180" /> 
+                                            : 
+                                        <IoIosArrowUp className="transition-transform duration-300 transform rotate-0" />
+                                }
+                            </div>
+                            {
+                            openDropdown === 'blogs' &&
+                                <div className='absolute top-10 left-0 bg-red-100 p-3'>
+                                    <li>nice</li>
+                                    <li>nice</li>
+                                    <li>nice</li>
+                                </div>
+                            }
+                        </div>
+                    </div>
                 </div>
                 <div>
                     <Button text='Contact' 
-                    buttonStyles='group-hover:text-black text-yellow-500 border-2 border-blue-500  ' 
-                    hoverStyles='group-hover:bg-blue-700 ' 
+                    styles="bg-customBlue border-2 border-opacity-0 border-customBlue-border hover:border-opacity-100 hover:bg-white hover:text-black"
                     />
                 </div>
             </div>
